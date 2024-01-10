df55b8e3c41bf1a5a36e54e896f5aff0eefbae80
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-01-10 00:09:31 +0000

    css updated

--- app/components/product/ProductCard.tsx
+++ app/components/product/ProductCard.tsx
@@ -5,33 +5,66 @@ import { userSchema } from "@/app/validation/Uservalid";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-
 export default function ProductCard() {
- const {register, handleSubmit,formState:{ errors }, reset } = useForm({
-  resolver: yupResolver(userSchema),
- });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(userSchema),
+  });
   const onSubmitHandler = (data: any) => {
     console.log({ data });
     reset();
-  }
- 
- 
-  
-  return (
-    <div className={style.App}>
-      <form onSubmit={handleSubmit(onSubmitHandler)} className={style.form}>
-        <input type="text" required placeholder="First Name.." {...register('firstName')} />
-          <p>{errors.firstName?.message}</p>
-        <input type="text" required placeholder="Last Name.." {...register('lastName')}/>
-        <p>{errors.lastName?.message}</p>
-        <input type="text" required placeholder="felix@gmail.com" {...register('email')} />
-        <p>{errors.email?.message}</p>
-        <input type="text" required placeholder="Password" {...register('password')}/>
-        <p>{errors.password?.message}</p>
-        <input type="text" required placeholder="Confirm Password" {...register('confirmPassword')} />
-        <p>{errors.confirmPassword?.message}</p>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
   };
+
+  return (
+    <>
+      
+      <div className={style.App}>
+      <h1>Account Login</h1>
+        <form onSubmit={handleSubmit(onSubmitHandler)} className={style.form}>
+          <input
+            type="text"
+            required
+            placeholder="First Name.."
+            {...register("firstName")}
+          />
+          <p>{errors.firstName?.message}</p>
+          <input
+            type="text"
+            required
+            placeholder="Last Name.."
+            {...register("lastName")}
+          />
+          <p>{errors.lastName?.message}</p>
+          <input
+            type="text"
+            required
+            placeholder="felix@gmail.com"
+            {...register("email")}
+          />
+          <p>{errors.email?.message}</p>
+          <input
+            type="text"
+            required
+            placeholder="Password"
+            {...register("password")}
+          />
+          <p>{errors.password?.message}</p>
+          <input
+            type="text"
+            required
+            placeholder="Confirm Password"
+            {...register("confirmPassword")}
+          />
+          <p>{errors.confirmPassword?.message}</p>
+          <button type="submit" className={style.submit}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function home() {
   return (
     <>
     <main>
-      <h1>hello Nigeria</h1>
+    
     <ProductCard />
     </main>
     </>
